eaabc9039a02345bed3418192e2f3b6cef4df138
gabriel-skinny <gabrielcatoni12@gmail.com.br>
2021-09-14 09:12:05 -0300

    iniciando variaveis e criando/implementando a regra de negocio do codigo

--- script.py
+++ script.py
@@ -1,1 +1,17 @@
 # Escrever o codigo aqui
+SalaAtual = 1
+contadorDeTentativas = 0
+perdeu = False
+
+def Escolhercaminho():
+  global SalaAtual
+  global contadorDeTentativas
+  global perdeu
+
+
+  contadorDeTentativas += 1
+
+while (SalaAtual < 9):
+  Escolhercaminho()
+else:
+  print("Voce conseguiu sair da dungeon com apenas {} tentativas".format(contadorDeTentativas))
