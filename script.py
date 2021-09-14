7323b1b0e4adb7b80cc209b00ecd4d79007b0f93
gabriel-skinny <gabrielcatoni12@gmail.com.br>
2021-09-14 09:23:21 -0300

    corrigin nome de variaveis

--- script.py
+++ script.py
@@ -1,17 +1,17 @@
 # Escrever o codigo aqui
-SalaAtual = 1
+salaAtual = 1
 contadorDeTentativas = 0
 perdeu = False
 
 def Escolhercaminho():
-  global SalaAtual
+  global salaAtual
   global contadorDeTentativas
   global perdeu
 
 
   contadorDeTentativas += 1
 
-while (SalaAtual < 9):
+while (salaAtual < 9):
   Escolhercaminho()
 else:
   print("Voce conseguiu sair da dungeon com apenas {} tentativas".format(contadorDeTentativas))
