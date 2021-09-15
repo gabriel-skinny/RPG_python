f556c3f5b13f24a5d4f56981d4211f91428b5d78
gabriel-skinny <gabrielcatoni12@gmail.com.br>
2021-09-15 12:16:08 -0300

    :recycle: Abstraindo os prints para uma funçao que retorna um input

--- script.py
+++ script.py
@@ -7,18 +7,33 @@ perdeu = False
 jogo = True
 personagem = 0
 
+def PegarInputs(tipo):
+  if (tipo == "personagem"):
+    print("Escolha o seu personagem")
+    print("[1] - Guerreiro")
+    print("[2] - Arqueiro ")
+    print("[3] - Ladingo")
+    print("[4] - Mago \n ")
+
+    return int(input())
+
+  elif (tipo == "caminho"):
+    print("\nVoce esta na sala {}".format(salaAtual))
+    print("Escolha seu caminho:")
+    print("[1] - Caminho Vermelho")
+    print("[2] - Caminho Preto\n")
+
+    return int(input())
+
+  else:
+    print("ERROR!!")
+
 def EscolherPersonagem():
   global personagem
   global jogo
   global perdeu
   
-  print("Escolha o seu personagem")
-  print("[1] - Guerreiro")
-  print("[2] - Arqueiro ")
-  print("[3] - Ladingo")
-  print("[4] - Mago \n ")
-
-  personagem = int(input())
+  personagem = PegarInputs("personagem")
 
   if (personagem > 4 or personagem < 1):
     print("\n\n\nERROR NAO EXISTE ESSE PERSONAGEM QUE FOI DIGITADO!!\n")
@@ -30,12 +45,9 @@ def Escolhercaminho():
   global salaAtual
   global contadorDeTentativas
   global perdeu
+  global jogo
 
-  print("\nVoce esta na sala {}".format(salaAtual))
-  print("Escolha seu caminho:")
-  print("[1] - Caminho Vermelho")
-  print("[2] - Caminho Preto\n")
-  caminhoEscolhido = int(input())
+  caminhoEscolhido = PegarInputs("caminho")
 
   contadorDeTentativas += 1
 
